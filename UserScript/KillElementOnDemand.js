4334d15d8d2e5dcea3e2ba20571a1d59971c3d1f
vince-koch <vince.koch@gmail.com>
2022-05-18 21:56:08 -0400

    Swap tabs for spaces

--- UserScript/KillElementOnDemand.js
+++ UserScript/KillElementOnDemand.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kill Element On Demand
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Removes the element directly under the cursor when the backtick (`) key is pressed!
 // @author       Vince Koch
 // @match        https://*/*
@@ -16,10 +16,10 @@
     document.addEventListener("keydown", function onKeyDown(event) {
         if (event.ctrlKey === true && event.key === "`") {
             var elements = document.querySelectorAll(":hover");
-			if (elements.length > 0) {
-				var element = elements[elements.length - 1]
-				element.remove()
-			}
+            if (elements.length > 0) {
+                var element = elements[elements.length - 1];
+                element.remove();
+            }
         }
     });
 
