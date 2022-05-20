36afcf749820c2c85314f9629b1bb9af3c0d3246
vince-koch <vince.koch@gmail.com>
2022-05-20 12:15:57 -0400

    CTRL=Glow, CTRL+`=Kill

--- UserScript/KillElementOnDemand.js
+++ UserScript/KillElementOnDemand.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Kill Element On Demand
 // @namespace    http://tampermonkey.net/
-// @version      0.4
-// @description  Removes the element directly under the cursor when the backtick (`) key is pressed!
+// @version      0.5
+// @description  Highlights the element directly under the cursor when the CTRL is pressed, and kills it when CTRL+` is pressed!
 // @author       Vince Koch
 // @match        https://*/*
 // @match        http://*/*
@@ -13,14 +13,42 @@
 (function() {
     'use strict';
 
+    function addGlowStyle() {
+        const style = document.createElement("style");
+        style.innerHTML = `
+            .kill-glow {
+                border: 2px solid red;
+                border-radius: 7px;
+                outline: none;
+                box-shadow: 0 0 10px red;
+            }`;
+
+        document.head.appendChild(style);
+    }
+
+    addGlowStyle();
+
+    let glowingElement = null;
+
     document.addEventListener("keydown", function onKeyDown(event) {
         if (event.ctrlKey === true && event.key === "`") {
-            var elements = document.querySelectorAll(":hover");
+            if (glowingElement !== null) {
+                glowingElement.remove();
+                glowingElement = null;
+            }
+        }
+        else if (event.ctrlKey === true) {
+            // remove glow from prior element
+            if (glowingElement !== null) {
+                glowingElement.classList.remove("kill-glow");
+            }
+
+            // find the current element and give it a glow
+            let elements = document.querySelectorAll(":hover");
             if (elements.length > 0) {
-                var element = elements[elements.length - 1];
-                element.remove();
+                glowingElement = elements[elements.length - 1];
+                glowingElement.classList.add("kill-glow");
             }
         }
     });
-
 })();
