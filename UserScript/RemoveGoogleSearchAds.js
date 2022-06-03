727a5aeab6a51efe2e53542214a9a3967b004189
vince-koch <vince.koch@gmail.com>
2022-06-03 02:17:54 -0400

    Update RemoveGoogleSearchAds.js

--- UserScript/RemoveGoogleSearchAds.js
+++ UserScript/RemoveGoogleSearchAds.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove Google Search Ads
 // @namespace    http://tampermonkey.net/
-// @version      0.8
+// @version      1.01
 // @description  Remove ads from Google search results
 // @author       vince-koch
 // @match        https://www.google.com/*
@@ -14,11 +14,23 @@
 (function() {
     'use strict';
 
+    const options = {
+        style: {
+            main: {
+                background: "red",
+                color: "white",
+            },
+        },
+        settings: {
+            duration: 2000,
+        },
+    };
+
     function removeAdsById(elementId) {
         var element = document.getElementById(elementId)
         if (element) {
             //element.remove();
-            const message = "RemoveGoogleSearchAds.js: Removed ads (" + elementId + ")";
+            const message = "Remove Google Search Ads ==> Removed ads (" + elementId + ")";
 
             const div = document.createElement("h4");
             const divContent = document.createTextNode(message);
@@ -26,11 +38,16 @@
             div.style.color = "red";
             element.parentNode.replaceChild(div, element);
 
-            console.warn(message);
+            console.info(message);
+            //iqwerty.toast.toast(message, options);
         }
     }
-    
-    console.warn("RemoveGoogleSearchAds.js");
+
+    console.info("Remove Google Search Ads ==> loaded");
+
+    // @require      https://raw.githubusercontent.com/mlcheng/js-toast/master/toast.min.js
+    //iqwerty.toast.toast("RemoveGoogleSearchAds.js loaded!", options);
+
     removeAdsById("tads");
     removeAdsById("bottomads");
 })();
