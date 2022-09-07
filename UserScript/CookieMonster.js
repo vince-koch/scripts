3b945602dc529ff51ba128b0964c37a1aca9d8d5
vince-koch <vince.koch@gmail.com>
2022-09-07 02:16:02 -0400

    Update CookieMonster.js

--- UserScript/CookieMonster.js
+++ UserScript/CookieMonster.js
@@ -1,29 +1,69 @@
 // ==UserScript==
 // @name         Cookie Monster
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Lots of sites are asking about cookies these days.  I just want to read something real quick.
 // @author       You
 // @match        https://*.askubuntu.com/*
 // @match        https://*.stackexchange.com/*
 // @match        https://*.stackoverflow.com/*
 // @match        https://*.superuser.com/*
+// @match        https://*.superuser.com/*
+// @match        https://*.wowhead.com/*
 // @icon         https://raw.githubusercontent.com/vince-koch/scripts/main/UserScript/cookie.ico
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+'use strict';
 
-    console.info("Scanning for annoying consent requests");
-    var divs = document.querySelectorAll('div.js-consent-banner');
-    for (var i = 0; i < divs.length; i++)
-    {
-        divs[i].remove();
+(function() {
+    function isIterable(obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        return typeof obj[Symbol.iterator] === 'function';
     }
 
-    if (divs.length > 0)
-    {
-        console.info("Removed " + divs.length + " annoyances");
+    function removeElements(elements) {
+        if (elements === null) {
+            return;
+        }
+
+        if (!isIterable(elements)) {
+            console.info("Cookie Monster is removing element", elements);
+            elements.remove();
+        }
+
+        // remove elements in reverse order
+        for (let i = elements.length - 1; i >= 0; --i) {
+            console.info("Cookie Monster is removing element", elements[i]);
+            elements[i].remove();
+        }
+    }
+
+    function scan(callback, interval, remainingAttempts) {
+        callback();
+        if (remainingAttempts > 0) {
+            setTimeout(scan, interval, callback, interval, remainingAttempts - 1);
+        }
+    }
+
+    function wowhead() {
+        // no privacy / cookie notifications
+        removeElements(document.getElementById("onetrust-consent-sdk"));
+
+        // no desktop notifications
+        removeElements(document.getElementsByClassName("notifications-dialog"));
+
+        // block video
+        removeElements(document.getElementsByClassName("blocks"));
+    }
+
+    console.info("Cookie Monster is scanning for annoyances");
+    removeElements(document.querySelectorAll('div.js-consent-banner'));
+
+    if (window.location.host === "www.wowhead.com") {
+        scan(wowhead, 500, 10);
     }
 })();
