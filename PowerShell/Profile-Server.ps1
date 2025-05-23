e9fd48f79cb3f1654a409459776f4049e81a0dbd
Vince Koch <vince.koch@gmail.com>
2025-05-23 01:54:05 -0400

    remove pscolors; add write-colors

--- PowerShell/Profile-Server.ps1
+++ PowerShell/Profile-Server.ps1
@@ -30,9 +30,9 @@ function Install-Files {
         Invoke-WebRequest -Uri $rawUrl -OutFile $outputPath -UseBasicParsing -ErrorAction Stop
     }
 
-    # install modules
-    Write-Host "Installing PSColors"
-    Install-Module PSColors -Force -AllowClobber
+    # # install modules
+    # Write-Host "Installing PSColors"
+    # Install-Module PSColors -Force -AllowClobber
 
     # create $profile
     Write-Host "Creating `$profile"
@@ -91,6 +91,19 @@ function Try-Import-Module {
 }
 
 
+function Write-Colors {
+    $colors = [Enum]::GetValues([System.ConsoleColor])
+
+    foreach ($fg in $colors) {
+        foreach ($bg in $colors) {
+            Write-Host (" FG:{0,-10} BG:{1,-10} " -f $fg, $bg) -ForegroundColor $fg -BackgroundColor $bg -NoNewline
+            Write-Host "  "
+        }
+        Write-Host "`n"
+    }
+}
+
+
 function CustomPrompt {
 	$lastExit = if ($?) { "✓" } else { "✗" }
     $lastExitColor = if ($?) { "Green" } else { "Red" }
@@ -108,8 +121,6 @@ function CustomPrompt {
 
 
 if ($MyInvocation.MyCommand.Path -eq $null) {
-    Write-Host "Checkpoint 6" -ForegroundColor Green
-
     Welcome
     
     $files = @(
@@ -122,22 +133,12 @@ if ($MyInvocation.MyCommand.Path -eq $null) {
 
     Install-Files -Files $files -ProfileFile "Profile-Server.ps1"
 
-    Write-Host "Checkpoint 8" -ForegroundColor Green
-
     . $profile
-
-    Write-Host "Checkpoint 9" -ForegroundColor Green
 }
 else {
-    Write-Host "Checkpoint 10" -ForegroundColor Green
-
     Welcome
 
-    Write-Host "Checkpoint 11" -ForegroundColor Green
-
-    Import-Module PSColors # https://mtreit.com/powershell/2019/02/11/ATouchOfColor.html
-
-    Write-Host "Checkpoint 11.5" -ForegroundColor Green
+    # Import-Module PSColors # https://mtreit.com/powershell/2019/02/11/ATouchOfColor.html
 
     Try-Import-Module $PSScriptRoot\Aws.psm1
     Try-Import-Module $PSScriptRoot\Bookmark.psm1
@@ -146,11 +147,5 @@ else {
     $env:Path += ";$PSScriptRoot"
     $env:Path += ";D:\ETL\Tools\aws\2.15.12"
 
-    Write-Host "Checkpoint 12" -ForegroundColor Green
-
     function Prompt { CustomPrompt }
-
-    Write-Host "Checkpoint 13" -ForegroundColor Green
 }
-
-Write-Host "Checkpoint 4" -ForegroundColor Green
