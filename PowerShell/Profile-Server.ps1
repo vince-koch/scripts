b82450c400514ca2135c1b2026a15ebbe9a369df
Vince Koch <vince.koch@gmail.com>
2025-05-23 01:39:06 -0400

    uncomment some things

--- PowerShell/Profile-Server.ps1
+++ PowerShell/Profile-Server.ps1
@@ -1,7 +1,5 @@
 # Invoke-Expression (& { (Invoke-WebRequest -Uri https://raw.githubusercontent.com/vince-koch/scripts/refs/heads/main/PowerShell/Profile-Server.ps1 -UseBasicParsing).Content })
 
-Write-Host "Checkpoint 1" -ForegroundColor Green
-
 
 function Welcome {
     $edition = if ($PSVersionTable.PSEdition -eq "Desktop") { "Windows PowerShell" } else { "PowerShell Core" }
@@ -11,7 +9,6 @@ function Welcome {
     Write-Host "$([System.IO.Path]::GetFileName($PSCommandPath))" -ForegroundColor Cyan
 }
 
-Write-Host "Checkpoint 2" -ForegroundColor Green
 
 function Install-Files {
     param (
@@ -25,6 +22,7 @@ function Install-Files {
 
     # download files
     foreach ($file in $Files) {
+        Write-Host "Downloading $($file)"
         $rawUrl = "https://raw.githubusercontent.com/vince-koch/scripts/refs/heads/main/PowerShell/$($file)"
         $outputPath = [System.IO.Path]::Combine($Home, ".ps-env", $file)
         $outputDirectory = [System.IO.Path]::GetDirectoryName($outputPath)
@@ -33,16 +31,17 @@ function Install-Files {
     }
 
     # install modules
+    Write-Host "Installing PSColors"
     Install-Module PSColors -Force -AllowClobber
 
     # create $profile
+    Write-Host "Creating `$profile"
     $psEnvProfileFile = [System.IO.Path]::Combine($psEnvDirectory, $ProfileFile)
     $profileDirectory = [System.IO.Path]::GetDirectoryName($profile)
     $null = [System.IO.Directory]::CreateDirectory($profileDirectory)
     $null = [System.IO.File]::WriteAllText("$($profile)", ". $($psEnvProfileFile)")
 }
 
-Write-Host "Checkpoint 3" -ForegroundColor Green
 
 function Try-Import-Module {
     param (
@@ -52,18 +51,17 @@ function Try-Import-Module {
     $IsDebug = $false
     $ModuleName = [System.IO.Path]::GetFileNameWithoutExtension($ModulePath)
     
-    #if (-not (Get-Module -Name $ModuleName)) {
-    #    try {
-    #        Import-Module -Name $ModulePath -DisableNameChecking -Force -ErrorAction Stop
-    #        Write-Host "Loaded module " -ForegroundColor DarkGray -NoNewLine; Write-Host $ModuleName
-    #    }
-    #    catch {
-    #        Write-Host "Failed to import module from path '$ModulePath': $_" -ForegroundColor Red
-    #    }
-    #}
+    if (-not (Get-Module -Name $ModuleName)) {
+        try {
+            Import-Module -Name $ModulePath -DisableNameChecking -Force -ErrorAction Stop
+            Write-Host "Loaded module " -ForegroundColor DarkGray -NoNewLine; Write-Host $ModuleName
+        }
+        catch {
+            Write-Host "Failed to import module from path '$ModulePath': $_" -ForegroundColor Red
+        }
+    }
 }
 
-Write-Host "Checkpoint 4" -ForegroundColor Green
 
 function CustomPrompt {
 	$lastExit = if ($?) { "✓" } else { "✗" }
@@ -73,22 +71,19 @@ function CustomPrompt {
     Write-Host " $lastExit" -ForegroundColor $lastExitColor -NoNewLine                          # last exit code
     Write-Host " $([System.Environment]::UserName)" -ForegroundColor Magenta -NoNewLine         # user name
     Write-Host " $([System.Environment]::MachineName)" -ForegroundColor DarkMagenta -NoNewLine  # machine name
-    Write-Host " $((Get-Date).ToString("HH:mm"))" -ForegroundColor DarkGray -NoNewLine          # current time
+    Write-Host " $((Get-Date).ToString("HH:mm"))" -ForegroundColor Gray -NoNewLine              # current time
     Write-Host " $((Get-Location).Path)" -ForegroundColor Cyan -NoNewLine                       # current folder
     Write-Host ">" -ForegroundColor Blue -NoNewLine                                             # prompt indicator
 
     Return " "
 }
 
-Write-Host "Checkpoint 5" -ForegroundColor Green
 
 if ($MyInvocation.MyCommand.Path -eq $null) {
     Write-Host "Checkpoint 6" -ForegroundColor Green
 
     Welcome
     
-    Write-Host "Checkpoint 7" -ForegroundColor Green
-
     $files = @(
         "Aws.psm1",
         "Bookmark.psm1",
